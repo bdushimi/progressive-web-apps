5568ac64b38cb359791a3c5c4ed4ebc96f148f33
bdushimi <lespharaons@hotmail.com>
2022-06-17 11:09:34 +0200

    adds cache strategies

---
+++ workbox-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	"globDirectory": "build/",
+	"globPatterns": [
+		"**/*.css",
+		"index.html",
+		"js/animation.js",
+		"images/home/*.jpg",
+		"images/icon/*.svg"
+	],
+	"swSrc": "src/sw.js",
+	"swDest": "build/sw.js",
+	"globIgnores": [
+		"../workbox-config.js"
+	]
+};
